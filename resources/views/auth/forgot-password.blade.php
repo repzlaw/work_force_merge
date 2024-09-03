1394899ace12234e8c055f8b5208e1f5e265ad98
samuel ibitoye <ibitoyesamuel25@gmail.com>
2024-09-03 16:51:25 +0100

    Refactor forgot password

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,25 +1,58 @@
-<x-guest-layout>
-    <div class="mb-4 text-sm text-gray-600 dark:text-gray-400">
-        {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Forgot Password</title>
+    <link rel="stylesheet" href="../src/output.css" />
+</head>
+
+<body>
+    <div class="grid grid-cols-2">
+        <div class="col-md-6">
+            <div class="container">
+                <div class="box_container user_page mt-24">
+                    <h1 class="gray-800 text-3xl font-bold mb-6">Forgot Password</h1>
+                    <p class="gray-500 text-15 font-base">
+                        Enter the code we sent to your email
+                    </p>
+                    <!-- Session Status -->
+                    <x-auth-session-status class="mb-4" :status="session('status')" />
+
+                    <form method="POST" action="{{ route('password.email') }}">
+                        @csrf
+                        <div class="user_form mt-6" id="talent_form">
+                            <div class="form-control">
+                                <label for="email">Your email address</label>
+                                <div class="">
+                                    <x-input-error :messages="$errors->get('email')" class="mt-2" />
+
+                                    <input type="email" name="email" id="email"
+                                        class="bc-gray-300 opacity-20 form-input" />
+                                </div>
+                            </div>
+                        </div>
+                        <div class="my-4">
+                            <button type="submit" class="btn-blue btn">Continue</button>
+                        </div>
+                    </form>
+
+
+                    <div class="continue_txt mt-6">
+                        <span>By clicking continue, you agree to our
+                            <a href="#">Terms of Service </a><span> and </span><a href="#">Privacy
+                                Policy</a>
+                        </span>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="logo_banner">
+            <img src="../src/assets/left-side-banner.png" alt="" />
+        </div>
     </div>
+    <script src="../src/js/index.js"></script>
+</body>
 
-    <!-- Session Status -->
-    <x-auth-session-status class="mb-4" :status="session('status')" />
-
-    <form method="POST" action="{{ route('password.email') }}">
-        @csrf
-
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            <x-primary-button>
-                {{ __('Email Password Reset Link') }}
-            </x-primary-button>
-        </div>
-    </form>
-</x-guest-layout>
+</html>
