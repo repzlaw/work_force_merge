2f2a8b8ebdda0d8137f705235b1c2953a84767fa
samuel ibitoye <ibitoyesamuel25@gmail.com>
2024-09-05 11:40:00 +0100

    update skills

--- resources/views/user/onboarding/skills.blade.php
+++ resources/views/user/onboarding/skills.blade.php
@@ -1,114 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
 
-<<<<<<< Updated upstream
-    <div class="py-12">
-        <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
-            <div class="overflow-hidden bg-white shadow-sm dark:bg-gray-100 sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
-                    @php
-                        $startingIndex = $user_skills ? count($user_skills->skills) : 0;
-                    @endphp
-                    <h4 class="mb-4 text-gray-900">What are you skills?</h4>
-                    
-                    <form method="POST" action="{{ route('onboarding.skills.store') }}">
-                        @csrf
-                        
-                        <div id="skill-container">
-                            @isset($user_skills)
-                                @foreach ($user_skills->skills as $index => $individual_skill)
-                                
-                                    <div class="pb-5 my-4 mb-5 skill-block" id="skill-block-{{$index}}">
-                                        {{-- <input type="hidden" name="skills[{{$index}}][id]" value="{{ $skill->id }}"> --}}
-
-                                        <div class="sm:col-span-3">
-                                            <label for="skill{{$index}}" class="block text-sm font-medium leading-6 text-gray-900">
-                                                Skill
-                                            </label>
-                                            <div class="mt-2">
-                                                <select id="skill" name="skills[{{$index}}][skill]" autocomplete="skill-name" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6"
-                                                required>
-                                                    <option value="">Select a skill</option>
-                                                    @foreach ($skills as $skill)
-                                                        <option value="{{ $skill->id }}" {{ $skill->id == $individual_skill['skill'] ? 'selected' : '' }}>{{ $skill->name }}</option>
-                                                    @endforeach
-                                                </select>
-                                            </div>
-                                        </div>
-
-                                        <div class="mt-4 sm:col-span-3">
-                                            <label for="rating{{$index}}" class="block text-sm font-medium leading-6 text-gray-900">
-                                                What would you rate yourself?
-                                            </label>
-                                            <div class="mt-2">
-                                                <input type="number" value="{{ $individual_skill['rating'] }}" name="skills[{{$index}}][rating]" id="rating{{$index}}" autocomplete="off" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" required>
-                                            </div>
-                                        </div>
-
-                                        <div class="mt-4 sm:col-span-3">
-                                            <label for="experience{{$index}}" class="block text-sm font-medium leading-6 text-gray-900">
-                                                How many years of experience do you have with this skill?
-                                            </label>
-                                            <div class="mt-2">
-                                                <input type="number" value="{{ $individual_skill['experience'] }}" name="skills[{{$index}}][experience]" id="experience{{$index}}" autocomplete="off" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" required>
-                                            </div>
-                                        </div>
-
-                                        <button type="button" data-skill-id="{{ $index }}" class="px-3 py-2 my-4 text-sm font-semibold text-white bg-red-500 rounded-md shadow-sm delete-skill-btn hover:bg-red-600" data-index="{{$index}}">Delete</button>
-                                        <br/>
-                                    </div>
-                                @endforeach
-                                
-                            @endisset
-
-                            @if ($startingIndex == 0)
-                                <div class="pb-5 my-4 mb-5 skill-block-0" id="skill-block-0">
-                                    <div id="new-skill mb-4">
-                                        <input type="hidden" name="skills[0][id]" value="null">
-
-                                        <div class="sm:col-span-3">
-                                            <label for="skill" class="block text-sm font-medium leading-6 text-gray-900">
-                                                Skill
-                                            </label>
-                                            {{-- <div class="mt-2">
-                                            <input type="text" name="skills[0][skill]" id="skill" autocomplete="given-name" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                                            required> --}}
-                                            <div class="mt-2">
-                                                <select id="skill" name="skills[0][skill]" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6"
-                                                required>
-                                                    <option value="">Select a skill</option>
-                                                    @foreach ($skills as $skill)
-                                                        <option value="{{ $skill->id }}">{{ $skill->name }}</option>
-                                                    @endforeach
-                                                </select>
-                                            </div>
-                                            </div>
-                                        </div>
-                
-                                        <div class="mt-4 sm:col-span-3">
-                                            <label for="rating" class="block text-sm font-medium leading-6 text-gray-900">
-                                                What would you rate yourself?
-                                            </label>
-                                            <div class="mt-2">
-                                            <input type="number" name="skills[0][rating]" id="rating" autocomplete="given-name" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                                            required>
-                                            </div>
-                                        </div>
-                
-                                        <div class="mt-4 sm:col-span-3">
-                                            <label for="experience" class="block text-sm font-medium leading-6 text-gray-900">
-                                                How many years of experience do you have with this skill?
-                                            </label>
-                                            <div class="mt-2">
-                                            <input type="number" name="skills[0][experience]" id="experience" autocomplete="given-name" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                                            required>
-                                            </div>
-                                        </div>
-                
-                                    </div>
-                                    <button type="button" class="px-3 py-2 my-4 text-sm font-semibold text-white bg-red-500 rounded-md shadow-sm delete-skill-btn hover:bg-red-600" data-index="0">Delete</button>
-                                    <br/>
-=======
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
@@ -191,7 +83,6 @@
                                             Invision
                                         </li>
                                     </ul>
->>>>>>> Stashed changes
                                 </div>
                                 <div class="form-control mt-6">
                                     <label for="experience" class="gray-500 font-medium">How many years of experience do
@@ -324,17 +215,16 @@
             </div>
         </div>
     </div>
-<<<<<<< Updated upstream
     <!-- JavaScript to handle adding new skill blocks -->
     <script>
         $(document).ready(function() {
-            var currentIndex = {{ $startingIndex > 0 ? $startingIndex : 1 }};
-        
-            $('#add-skill').click(function() {
-                var newBlock = `
+                    var currentIndex = {{ $startingIndex > 0 ? $startingIndex : 1 }};
+
+                    $('#add-skill').click(function() {
+                        var newBlock = `
                     <div class="pb-5 my-4 mb-5 skill-block" id="skill-block-${currentIndex}">
                         <input type="hidden" name="skills[${currentIndex}][id]" value="null">
-                        
+
                         <div class="sm:col-span-3">
                             <label for="skill${currentIndex}" class="block text-sm font-medium leading-6 text-gray-900">Name of School</label>
                             <div class="mt-2">
@@ -342,12 +232,12 @@
                                 required>
                                     <option value="">Select a skill</option>
                                     @foreach ($skills as $skill)
-                                        <option value="{{ $skill->id }}">{{ $skill->name }}</option>
-                                    @endforeach
+<option value="{{ $skill->id }}">{{ $skill->name }}</option>
+@endforeach
                                 </select>
                             </div>
                         </div>
-        
+
                         <div class="mt-4 sm:col-span-3">
                             <label for="rating${currentIndex}" class="block text-sm font-medium leading-6 text-gray-900">
                                 What would you rate yourself?
@@ -356,7 +246,7 @@
                                 <input type="number" name="skills[${currentIndex}][rating]" id="rating${currentIndex}" autocomplete="off" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" required>
                             </div>
                         </div>
-        
+
                         <div class="mt-4 sm:col-span-3">
                             <label for="experience${currentIndex}" class="block text-sm font-medium leading-6 text-gray-900">
                             How many years of experience do you have with this skill?
@@ -365,23 +255,26 @@
                                 <input type="number" name="skills[${currentIndex}][experience]" id="experience${currentIndex}" autocomplete="off" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" required>
                             </div>
                         </div>
-        
+
                         <button type="button" class="px-3 py-2 my-4 text-sm font-semibold text-white bg-red-500 rounded-md shadow-sm delete-skill-btn hover:bg-red-600" data-index="${currentIndex}">Delete</button>
                         <br/>
                     </div>
                 `;
-                
-                $('#skill-container').append(newBlock);
-                
-                // Bind delete function to new button
-                $(`#skill-block-${currentIndex} .delete-skill-btn`).click(function() {
-                    $(this).parent().remove();
-                });
-                
-                currentIndex++;
-            });
-=======
-</body>
->>>>>>> Stashed changes
 
-</html>
+                        $('#skill-container').append(newBlock);
+
+                        // Bind delete function to new button
+                        $(`#skill-block-${currentIndex} .delete-skill-btn`).click(function() {
+                            $(this).parent().remove();
+                        });
+
+                        currentIndex++;
+                    });
+
+
+                    <
+                    /script> <
+
+                    /
+                    body > < /
+                    html >
